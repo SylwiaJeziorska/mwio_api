f7a4b4b6c99eaca3e79cb01d9def38355d8905d6
Sylwia Jeziorska <s.jeziorska@ratioweb.pl>
2020-06-14 15:23:16 +0200

    JWT instalation and base

---
+++ app/Http/Controllers/Auth/SignInController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SignInController extends Controller
+{
+   public function __invoke(Request $request)
+   {
+
+   $credentials = request(['email', 'password']);
+       if(!$token = auth()->attempt($credentials)){
+           return response(null,401);
+       }
+       return response()->json(compact('token'));
+   }
+}
